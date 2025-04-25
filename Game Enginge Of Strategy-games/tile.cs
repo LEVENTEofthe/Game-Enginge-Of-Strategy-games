4e8e88c5cb8fa04c7a3aab47dc30a453317cc9ac
LEVENTEofthe <126454907+LEVENTEofthe@users.noreply.github.com>
2025-04-25 15:10:51 +0200

    Now the map drawing app is also part of the git saves

--- Game Enginge Of Strategy-games/tile.cs
+++ Game Enginge Of Strategy-games/tile.cs
@@ -9,39 +9,35 @@ namespace Game_Enginge_Of_Strategy_games
 {
     internal class tile
     {
-        private int row;
         private int column;
+        private int row;
         public Image Texture { get; set; }
         public mapObject MapObject { get; set; }
         public actors ActorStandsHere { get; set; }
 
-
-        public tile(int Row, int Column, Image Texture)
+        public tile(int Column, int Row, Image Texture)
         {
             this.Row = Row;
             this.Column = Column;
             this.Texture = Texture;
         }
 
-        public tile(int Row, int Column)
+        public tile(int Column, int Row)
         {
-            this.Row = Row;
-            this.Column = Column;
-            Texture = Image.FromFile("C:/Users/bakos/Documents/GEOS assets/tiles/placeholder.png");
+            this.column = Column;
+            this.row = Row;
         }
 
-
-        public int Row
-        {
-            get { return row; }
-            set { row = value - 1; }    //-1 because otherwise we would have to count rows and columns starting from number 0
-        }
         public int Column
         {
             get { return column; }
-            set {  column = value - 1; }
+            set {  column = value - 1; }    //-1 because otherwise we would have to count rows and columns starting from number 0
         }
-
+        public int Row
+        {
+            get { return row; }
+            set { row = value - 1; }
+        }
 
         public bool CanStepHere()
         {
