85991160819ad38ba28267c4a0a4310850666868
LEVENTEofthe <126454907+LEVENTEofthe@users.noreply.github.com>
2025-04-25 15:49:04 +0200

    Revert "Now the map drawing app is also part of the git saves"
    This reverts commit 4e8e88c5cb8fa04c7a3aab47dc30a453317cc9ac.

--- Game Enginge Of Strategy-games/tile.cs
+++ Game Enginge Of Strategy-games/tile.cs
@@ -9,35 +9,39 @@ namespace Game_Enginge_Of_Strategy_games
 {
     internal class tile
     {
-        private int column;
         private int row;
+        private int column;
         public Image Texture { get; set; }
         public mapObject MapObject { get; set; }
         public actors ActorStandsHere { get; set; }
 
-        public tile(int Column, int Row, Image Texture)
+
+        public tile(int Row, int Column, Image Texture)
         {
             this.Row = Row;
             this.Column = Column;
             this.Texture = Texture;
         }
 
-        public tile(int Column, int Row)
+        public tile(int Row, int Column)
         {
-            this.column = Column;
-            this.row = Row;
+            this.Row = Row;
+            this.Column = Column;
+            Texture = Image.FromFile("C:/Users/bakos/Documents/GEOS assets/tiles/placeholder.png");
         }
 
-        public int Column
-        {
-            get { return column; }
-            set {  column = value - 1; }    //-1 because otherwise we would have to count rows and columns starting from number 0
-        }
+
         public int Row
         {
             get { return row; }
-            set { row = value - 1; }
+            set { row = value - 1; }    //-1 because otherwise we would have to count rows and columns starting from number 0
         }
+        public int Column
+        {
+            get { return column; }
+            set {  column = value - 1; }
+        }
+
 
         public bool CanStepHere()
         {
